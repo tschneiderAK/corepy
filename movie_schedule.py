a55d1b0846a5bcdce75e255ae42d1a6e40de0bc9
tschneiderAK <91346215+tschneiderAK@users.noreply.github.com>
2021-09-28 09:38:26 -0800

    Added words.py and movie_schedule.py
    words.py:
    This is the code I've been working on in the larger Core Python course.
    movie_schedule:
    This is the code from the Practical Python for Beginners course.

---
+++ movie_schedule.py
@@ -0,0 +1,39 @@
+current_movies = {}
+current_movies['The Grinch'] = '11:00AM'
+current_movies['Rudolph'] = '1:00PM'
+current_movies['Frosty'] = '3:00PM'
+current_movies['Vacation'] = '5:00PM'
+
+print('Currently showing:')
+for key in current_movies:
+    print(key)
+
+#set initial 'finished' status to 'N'. We will repeat this loop until the user indicates they are done.
+finished = 'N'
+finished_input = None
+
+#define acceptable answers for Yes/No. Only a Y or N will be allowed.
+answers = {'Y': 'Yes',
+           'N': 'No'}
+
+
+while finished == 'N':
+
+    movie = input('Which movie would you like a showtime for?\n')
+        
+    showtime = current_movies.get(movie)
+    if showtime == None:
+        print('This movie isn\'t playing.')
+    else:
+        print(showtime)
+
+    finished_input = input('Are you finished checking movies? Y/N\n')
+    
+    while answers.get(finished_input) == None:
+        finished_input = input('Invalid response. Are you finished? Y/N\n')
+
+    finished = finished_input
+
+    if finished == 'Y':
+        print('Thank you for using MovieCheck!')
+        break
---
+++ words.py
@@ -0,0 +1,51 @@
+"""Retrieve and print words from a URL.
+
+Usage:
+
+    python words.py <URL>
+    """
+
+import sys
+from urllib.request import urlopen
+
+
+def fetch_words(url):
+    """Fetches a list of words from a URL.
+    
+    Args:
+        url: The URL of a UTF-8 text document.
+        
+    Returns:
+        A list of strings containing the words from the document.
+    """
+    story = urlopen(url)
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf8').split()
+        for word in line_words:
+            story_words.append(word)
+    story.close()
+    return story_words
+
+
+def print_items(items):
+    """Prints items in a list.
+    
+    Args:
+        An iterable list of printable items.
+    """
+    for item in items:
+        print(item)
+
+
+def main(url):
+    """Prints each word from a text document at a URL.
+    
+    Args:
+        url: The URL of a UTF-8 text document."""
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])   # The 0th arg is the module filename.
